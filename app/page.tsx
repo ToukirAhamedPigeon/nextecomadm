2028660eb93f9c36059a57ca245af5c40ef1df50
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-04-03 17:50:12 +0600

    Clean Up the project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-normal">Hello World. This is Next E Commerce Admin App.</h1>
-    </div>
-  );
-}
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-center text-3xl font-normal">Hello World. This is Next E Commerce Admin App.</h1>
+      </div>
+    );
+  }
