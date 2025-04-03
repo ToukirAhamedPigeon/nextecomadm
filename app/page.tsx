990bd37aafcf02d3f3ada609f0e609a9c2e7d2aa
Toukir Ahamed Pigeon <toukir.ahamed.pigeon@gmail.com>
2025-04-03 18:05:49 +0600

    Font set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 export default function Home() {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-center text-3xl font-normal">Hello World. This is Next E Commerce Admin App.</h1>
+        <h1 className={cn("text-center text-3xl font-normal","text-blue-500","uppercase")}><Button variant="destructive" size="lg">Hello World</Button> This is Next E Commerce Admin App.</h1>
       </div>
     );
   }
